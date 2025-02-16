101dbf118cc787aca6e2269b766c0a6c1f34fde0
Jordan Bassett <jbassett2@students.columbiabasin.edu>
2025-02-16 12:41:05 -0800

    Added PasswordValidatorTest.java to...test added the PasswordChecker interface class added PasswordValidator class. Does minimal basic check on passwords added README and TODO

--- src/main/java/passwordfactory/PasswordValidatorTest.java
+++ src/main/java/passwordfactory/PasswordValidatorTest.java
@@ -1,18 +1,18 @@
-
 package passwordfactory;
-/**
- * Test program for password validation, separate from the GUI application.
- */
-public class PasswordValidatorTest {
-
-    public static void main(String[] args) {
+import javafx.application.Platform;
+//Test program for password validation, separate from the GUI
+public class PasswordValidatorTest
+{
+    public static void main(String[] args)
+    {
         PasswordValidator validator = new PasswordValidator();
         
         String[] testPasswords = {
             "123456", "Password123!", "HelloWorld!", "Str0ngP@ss!", "SuperSecure2025!", "qwerty1234"
         };
 
-        for (String pw : testPasswords) {
+        for (String pw : testPasswords)
+        {
             System.out.println("Testing: " + pw);
             System.out.println("Score: " + validator.checkStrength(pw)); // Show numeric score
             System.out.println("Feedback: " + validator.giveFeedback(pw));
